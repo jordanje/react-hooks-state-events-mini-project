a7aaf2ce80a982a75ed2afe9fb67e5ffe26ee739
Jordan Jennings <jordanjennings12@gmail.com>
2022-09-04 15:17:31 -0400

    commit tree 9b241c36d0e1367e270bed29947638d22d419577

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,32 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [ tasks, setTasks ] = useState(TASKS)
+  const [ category, setCategory ] = useState("All")
+  // const [filteredTasks , setFilteredTasks ] = useState([])
+
+  function onHandleDeleteTask(text){
+    const newList = tasks.filter(task => task.text !== text)
+    setTasks(newList)
+  }
+
+  const filteredTasks =  tasks.filter(
+    (task) => category === "All" || task.category === category
+  );
+
+  function onTaskFormSubmit(newTask, event){
+    event.preventDefault();
+    const newTaskList = [...tasks, newTask];
+    setTasks(newTaskList)
+  }
+  
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selectedCategory={category} onSelectCategory={setCategory}/>
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={filteredTasks} onHandleDeleteTask={onHandleDeleteTask} />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, onHandleDeleteTask}) {
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+     {tasks.map((task, index) => (
+    <Task key={index} 
+    category={task.category} 
+    text={task.text}
+     onHandleDeleteTask={onHandleDeleteTask}/>
+     )
+     )}
     </div>
   );
 }
